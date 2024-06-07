e4be932427b557f711512ce14d679f687eb178b5
SeryVathana <yooseryvathana@gmail.com>
2024-06-07 23:02:58 +0700

    Update MainLayout.tsx

--- frontend/src/Layouts/MainLayout.tsx
+++ frontend/src/Layouts/MainLayout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Footer from "../components/footer";
-import Navbar from "../components/navbar";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const MainLayout = () => {
   return (
